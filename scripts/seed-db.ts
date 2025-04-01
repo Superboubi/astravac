19b631e9dc2a22866b4d78462848e848b0097943
Saillour Léo <leosaillour@gmail.com>
2025-04-01 14:02:21 +0200

    fix: update eslint config and dotenv usage

--- scripts/seed-db.ts
+++ scripts/seed-db.ts
@@ -1,9 +1,12 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const rootDir = join(__dirname, '..')
+
+// Charger les variables d'environnement
+config()
 
 import '../src/lib/seed.ts' 
