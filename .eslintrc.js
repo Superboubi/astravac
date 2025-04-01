c0c324b893346ed1ce287d74f39dfd9564960941
Saillour Léo <leosaillour@gmail.com>
2025-04-01 12:00:06 +0200

    fix: replace eslint config and add dotenv

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['next'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/no-unescaped-entities': 'off',
+    '@next/next/no-img-element': 'warn',
+    'no-console': 'warn'
+  }
+}
